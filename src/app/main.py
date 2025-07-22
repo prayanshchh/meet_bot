07258cf234cb75812d84c15261a6d078aa3120ae
prayansh_chhablani <prayanshchhablani@gmail.com>
2025-07-23 00:11:33 +0530

    cors

--- src/app/main.py
+++ src/app/main.py
@@ -16,7 +16,7 @@ app.add_middleware(SessionMiddleware, secret_key=os.getenv("SECRET_KEY"))
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5173"],
+    allow_origins=["https://meetbot.prayanshchhablani.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
